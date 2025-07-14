e440d4278f97a8d49773e96628e4c84253266755
Felipe Faruth <felipefaruth@gmail.com>
2025-07-14 15:02:23 -0300

    Classe PessoaJuridica update

--- Impostos/src/entities/PessoaJuridica.java
+++ Impostos/src/entities/PessoaJuridica.java
@@ -1,5 +1,31 @@
 package entities;
 
-public class PessoaJuridica {
+public class PessoaJuridica extends Pessoa {
+	
+	private int num_func;
 
+	@Override
+	public double calcImposto() {
+		return 0;
+	}
+
+	public PessoaJuridica() {
+		super();
+	}
+	
+	public PessoaJuridica(String nome, double renda_anual, int num_func) {
+		super(nome, renda_anual);
+		this.num_func = num_func;
+	}
+
+	public int getNum_func() {
+		return num_func;
+	}
+
+	public void setNum_func(int num_func) {
+		this.num_func = num_func;
+	}
+	
+	
+	
 }
