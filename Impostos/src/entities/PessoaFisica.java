45a37bb6c65ef5776f454eebe36c4bff70c70932
Felipe Faruth <felipefaruth@gmail.com>
2025-07-14 15:07:11 -0300

    Classe PessoaFisica update

--- Impostos/src/entities/PessoaFisica.java
+++ Impostos/src/entities/PessoaFisica.java
@@ -1,5 +1,33 @@
 package entities;
 
-public class PessoaFisica {
+public class PessoaFisica extends Pessoa{
+
+	private double gastos_saude;
+	
+	
+	public PessoaFisica() {
+		super();
+	}
+	
+	public PessoaFisica(String nome, double renda_anual, double gastos_saude) {
+		super(nome, renda_anual);
+		this.gastos_saude = gastos_saude;
+	}
+
+
+	@Override
+	public double calcImposto() {
+		return 0;
+	}
+
+	public double getGastos_saude() {
+		return gastos_saude;
+	}
+
+	public void setGastos_saude(double gastos_saude) {
+		this.gastos_saude = gastos_saude;
+	}
+	
+	
 
 }
