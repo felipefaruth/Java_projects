6c1bd8c62fc1fc4ba33a7e456f125a34550e6820
Felipe Faruth <felipefaruth@gmail.com>
2025-07-14 14:55:50 -0300

    Métodos e atributos e classes Pessoa Fisica e Juridica

--- Impostos/src/entities/Pessoa.java
+++ Impostos/src/entities/Pessoa.java
@@ -1,5 +1,38 @@
 package entities;
 
-public class Pessoa {
+public abstract class Pessoa {
+	private String nome;
+	private double renda_anual;
+	
+	public Pessoa(){
+	}
+	
+	public Pessoa(String nome, double renda_anual) {
+		this.nome = nome;
+		this.renda_anual = renda_anual;
+	}
 
+	public abstract double calcImposto();
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public double getRenda_anual() {
+		return renda_anual;
+	}
+
+
+	public void setRenda_anual(double renda_anual) {
+		this.renda_anual = renda_anual;
+	}
+	
+	
 }
